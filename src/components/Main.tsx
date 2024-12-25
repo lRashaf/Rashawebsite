f1b547ef132419a272f5b7e39f2e7d24875b26c2
lrashaf <irasha.alzyadi@gmail.com>
2024-12-25 21:46:55 +0300

    Updated content/design

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,14 +14,15 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/lRashaf" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="linkedin.com/in/rasha-alzyadi" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/rasha-alzyadi/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Rasha Alzyadi </h1>
           <p>Web Developer | UX UI Designer</p>
 
           <div className="mobile_social_icons">
           <a href="https://github.com/IRashaf" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-           <a href="linkedin.com/in/rasha-alzyadi" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          <a href="https://www.linkedin.com/in/rasha-alzyadi/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+
           </div>
         </div>
       </div>
